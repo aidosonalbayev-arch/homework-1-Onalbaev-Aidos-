174f40ba281b731f7b465775229d3bb0713fec75
aidosonalbayev-arch <aidos.onalbayev@narxoz.kz>
2026-02-11 22:58:55 +0500

    Create Character interface with basic stats and equipment methods

--- src/main/java/com/narxoz/rpg/character/Character.java
+++ src/main/java/com/narxoz/rpg/character/Character.java
@@ -1,5 +1,8 @@
 package com.narxoz.rpg.character;
 
+import com.narxoz.rpg.equipment.Armor;
+import com.narxoz.rpg.equipment.Weapon;
+
 /**
  * Base interface for all character types in the RPG system.
  *
@@ -22,6 +25,19 @@ public interface Character {
     // - int getIntelligence()
     // - void displayStats()
     // - void useSpecialAbility()
+
+    String getName();
+    int getHealth();
+    int getMana();
+    int getStrength();
+    int getIntelligence();
+    void displayStats();
+    void useSpecialAbility();
+
+    void equipWeapon(Weapon weapon);
+    void equipArmor(Armor armor);  
+
+    void displayEquipment();
 
     // TODO: Think about equipment
     // Should characters know about their equipped items?
