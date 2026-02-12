a2863e62b9a677bbaadcf74d3b12396f7757a9b7
aidosonalbayev-arch <aidos.onalbayev@narxoz.kz>
2026-02-12 15:53:54 +0500

    Create Main.java to demo character creation and equipment

--- src/main/java/com/narxoz/rpg/Main.java
+++ src/main/java/com/narxoz/rpg/Main.java
@@ -1,5 +1,13 @@
 package com.narxoz.rpg;
 
+import com.narxoz.rpg.character.Archer;
+import com.narxoz.rpg.character.Mage;
+import com.narxoz.rpg.character.Warrior;
+import com.narxoz.rpg.equipment.EquipmentFactory;
+import com.narxoz.rpg.equipment.MagicFactory;
+import com.narxoz.rpg.equipment.MedievalFactory;
+import com.narxoz.rpg.equipment.RangerFactory;
+
 /**
  * Main demonstration class for the RPG Character & Equipment System.
  *
@@ -52,6 +60,40 @@ public class Main {
         // In comments, explain how easy it would be to:
         // - Add a new character class (e.g., Rogue, Paladin)
         // - Add a new equipment theme (e.g., Dragon Slayer, Undead)
+
+        Warrior warrior = new Warrior("Thorin");
+        Mage mage = new Mage("Gandalf");
+        Archer archer = new Archer("Legolas");
+
+        EquipmentFactory medieval = new MedievalFactory();
+        EquipmentFactory magic = new MagicFactory();
+        EquipmentFactory ranger = new RangerFactory();
+
+        warrior.equipWeapon(medieval.createWeapon());
+        warrior.equipArmor(medieval.createArmor());
+
+        mage.equipWeapon(magic.createWeapon());
+        mage.equipArmor(magic.createArmor());
+
+        archer.equipWeapon(ranger.createWeapon());
+        archer.equipArmor(ranger.createArmor());
+
+        warrior.displayStats();
+        warrior.displayEquipment();
+        warrior.useSpecialAbility();
+
+        System.out.println();
+
+        mage.displayStats();
+        mage.displayEquipment();
+        mage.useSpecialAbility();
+
+        System.out.println();
+
+        archer.displayStats();
+        archer.displayEquipment();
+        archer.useSpecialAbility();
+        
 
 
         System.out.println("\n=== Demo Complete ===");
