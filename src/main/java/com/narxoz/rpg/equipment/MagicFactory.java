28d828198e8070299dc11646e47511b0dc940e00
aidosonalbayev-arch <aidos.onalbayev@narxoz.kz>
2026-02-12 14:57:09 +0500

    Implement concrete equipment factories: Medieval, Magic, Ranger

---
+++ src/main/java/com/narxoz/rpg/equipment/MagicFactory.java
@@ -0,0 +1,15 @@
+package com.narxoz.rpg.equipment;
+
+public class MagicFactory implements EquipmentFactory {
+
+    @Override
+    public Weapon createWeapon() {
+        return new WizardStaff();
+    }
+
+    @Override
+    public Armor createArmor() {
+        return new EnchantedRobes();
+    }
+    
+}
